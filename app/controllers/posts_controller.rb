48c3b6b39254de1001e70044fc216205ed45e4ed
Junko Werner <aliciajace@gmail.com>
2015-08-30 21:21:16 -0700

    CRUD Posts index, show

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index
+  	@posts = Post.all
+  end
+
+  def new
+  end
+
+  def show
+     @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+end
